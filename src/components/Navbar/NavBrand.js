7e76dad1b3cb48e6ca2a0637cd1445827f01c76a
MenathNDGD <nadungmenath1@gmail.com>
2025-02-07 23:41:44 +0530

    Add NavBrand component for branding in the Navbar

---
+++ src/components/Navbar/NavBrand.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBrand = () => {
+  return (
+    <div className="flex items-center space-x-4">
+      <img
+        className="w-12 select-none"
+        src="../../assets/favicon.ico"
+        alt="logo"
+      />
+      <h1 className="text-3xl font-semibold text-blue-600 brand-font select-none">
+        MedLink
+      </h1>
+    </div>
+  );
+};
+
+export default NavBrand;
