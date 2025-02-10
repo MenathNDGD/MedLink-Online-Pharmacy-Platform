93e3bb0c15ade3c7a23b6ef74a802ee1f0367ef4
MenathNDGD <nadungmenath1@gmail.com>
2025-02-10 09:11:28 +0530

    Update avatar image and change name to MedLink in PromotionManagement component

--- client/src/pages/PromotionManagement/PromotionManagement.jsx
+++ client/src/pages/PromotionManagement/PromotionManagement.jsx
@@ -135,10 +135,10 @@ const generateReport = () => {
               <span>Download Report</span>
             </button>
             <div className='flex gap-2 cursor-pointer'>
-            <img className='w-12 h-12 border-2 border-white rounded-full' src="https://avatars.githubusercontent.com/u/125633025?v=4" alt="profile" />
+            <img className='w-12 h-12 border-2 border-white rounded-full' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="profile" />
             <div className="flex w-full flex-col gap-0.5">
               <div className="flex items-center justify-between font-bold">
-                <h1>Kavindu Dilshan</h1>
+                <h1>MedLink</h1>
               </div>
               <p className='text-xs '>Promotion Manager</p>
             </div>
